6b68c03881a32d76eac9d0e80baf6007123a8828
Angelo <gangelos630@gmail.com>
2024-03-15 18:50:03 -0500

    iniciando subida de proyecto - commons-microservices

---
+++ src/test/java/com/capas/microservicios/app/commons/CommonsMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capas.microservicios.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
